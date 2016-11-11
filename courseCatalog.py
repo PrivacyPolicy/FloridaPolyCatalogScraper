5b340f6afdbc8c3ff86dae89dd83880033cec185
Gabriel Hutchison <ghutchison2600@flpoly.org>
2016-11-11 11:57:41 -0500

    Added coreq scraping

--- courseCatalog.py
+++ courseCatalog.py
@@ -3,11 +3,12 @@ import re
 import html5lib
 from bs4 import BeautifulSoup
 
-DEBUG = True;
+DEBUG = False;
 
 semester = 7;
 #url = 'http://floridapolytechnic.catalog.acalog.com/preview_program.php?catoid=' + str(semester) + '&poid=401&returnto=302'
 url = 'file:C:/Users/Gabe/Documents/School/Etcetera/Scrapers/cache/preview_program.php&catoid=7&poid=401.html'
+global numberToID
 numberToID = {}
 
 def fileOut(data):
@@ -52,19 +53,16 @@ def getCourseData(id):
 
         credits = int(soup.find('strong').next_element.next_element)
 
-        # string = ''
         # all of this... ALL. OF. THIS. To get a nice list of prereqs
         try:
             a = soup.find_all('div', {'class': 'ajaxcourseindentfix'})[1]
             b = a.find('p') # <p> containing prereq data
             c = b.text.split('Prerequisites: ')[1] # text containing class list
-            d = str(c.encode('utf-8'))[2:-1] # remove anoying weird stuff at end
+            d = str(c.encode('utf-8'))[2:-1] # remove anoying weird stuff at ends
             e = d.replace('\\xc2\\xa0', '').replace('\\xc3\\x82', ' - ') # rmv weird chars
             f = e.replace(', ', ' and ').replace(' and or ', ' or ').replace(' and and ', ' and ') # some use commas instead of 'and'
             g = f.replace(' AND ', ' and ').replace(' OR ', ' or ') # fix capitalization inconsistancies
             # g = class data in string form
-
-            # string += '\n\n-' + str(id) + '-----------------------------\n\n' + prereqs
 
             a = g.split(' and ') # each of the potential courses
             array = '['
@@ -87,39 +85,68 @@ def getCourseData(id):
             if array == '':
                 array = 'null'
 
-            prereqs = array
-            # prereqs = prereq data in str(array) form
-
-
+            prereqs = array # prereqs = prereq data in str(array) form
         except IndexError:
             prereqs = 'null'
             if DEBUG: print('index error for id: ' + id)
             pass # doesn't even have Prerequisites
 
 
-        # if id == '3023':
-        #   print(string)
+        # Try to get all of the co-requisite data
+        try:
+            a = soup.find_all('div', {'class': 'ajaxcourseindentfix'})[1]
+            b = a.find_all('p')[1] # <p> containing coreq data
+            b1 = b.text.replace('Co-requisite or Prerequisite:', 'Co-requisite:') # for my uses, a prereq & coreq is the same as just a coreq
+            c = b1.split('Co-requisite: ')[1] # text containing class list
+            d = str(c.encode('utf-8'))[2:-1] # remove anoying weird stuff at ends
+            e = d.replace('\\xc2\\xa0', '').replace('\\xc3\\x82', ' - ') # rmv weird chars
+            f = e.replace(', ', ' and ').replace(' and or ', ' or ').replace(' and and ', ' and ') # some use commas instead of 'and'
+            g = f.replace(' AND ', ' and ').replace(' OR ', ' or ') # fix capitalization inconsistancies
+            # g = class data in string form
 
-        return {'id': id, 'number': number, 'name': name, 'credits': credits, 'prereqs': prereqs}
+            h = g.strip().replace(' ', '', 1) # remove first space...
+            i = h.split(' ')[0] # ...which should make this the course number
+            coreq = i # pass that because we haven't built the entire numberToID array yet
+        except IndexError:
+            coreq = 'null'
+            if DEBUG: print('index error for id: ' + id)
+            pass # doesn't even have Co-requisites
+
+
+        # if coreq == 'null': return '0'
+        return {'id': id, 'number': number, 'name': name, 'credits': credits, 'prereqs': prereqs, 'coreq': coreq}
 
 
 with urllib.request.urlopen(url) as response:
-   html = response.read()
+    html = response.read()
 
-   soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
 
-   global numberToID
-   for i in soup.find_all('li'):
-       try:
-           if i.attrs['class'][0] == 'acalog-course':
-               a = i.contents[0]
-               b = a.contents[0]
-               c = b.attrs['onclick']
-               d = c.replace(' ', '')
-               e = d.split("','")[1]
-               f = e.split(',')[0].split("'")[0]
-               course = getCourseData(f)
-               print(course)
-       except KeyError:
-           pass
-   # print(numberToID)
+    courseList = [];
+    # build course list
+    for i in soup.find_all('li'):
+        try:
+            if i.attrs['class'][0] == 'acalog-course':
+                a = i.contents[0]
+                b = a.contents[0]
+                c = b.attrs['onclick']
+                d = c.replace(' ', '')
+                e = d.split("','")[1]
+                f = e.split(',')[0].split("'")[0]
+                course = getCourseData(f)
+                if course != 0: courseList.append(course)
+        except KeyError:
+            pass
+
+    # convert coreq temp ids to coreq internal ids
+    for course in courseList:
+        try:
+            course['coreq'] = numberToID[course['coreq']]
+        except KeyError:
+            if DEBUG: print('No coreq in course ' + course['id'])
+            pass #
+
+    # find elective groups
+
+    # TODO convert to JSON
+    print(courseList)

138e98acfc0a56fff66c333d9bfe426c3aff3eba
Gabriel Hutchison <ghutchison2600@flpoly.org>
2016-11-11 10:37:31 -0500

    Finished prereqs scraping

--- courseCatalog.py
+++ courseCatalog.py
@@ -3,9 +3,12 @@ import re
 import html5lib
 from bs4 import BeautifulSoup
 
+DEBUG = True;
+
 semester = 7;
 #url = 'http://floridapolytechnic.catalog.acalog.com/preview_program.php?catoid=' + str(semester) + '&poid=401&returnto=302'
 url = 'file:C:/Users/Gabe/Documents/School/Etcetera/Scrapers/cache/preview_program.php&catoid=7&poid=401.html'
+numberToID = {}
 
 def fileOut(data):
     f = open('output.txt', 'w')
@@ -16,10 +19,24 @@ def fileOut(data):
 def arrToStr(arr):
     string = '['
     for el in arr:
-        string += str(el.encode('utf-8')) + ', '
+        string += el + ', '
     return string[:-2] + ']'
 
+def getClassID(data):
+    global numberToID
+    a = data.split(' - ')[0] # potentially the course number
+    b = a.replace(' ', '') # remove the space
+    try: # have we seen this class before?
+        id = numberToID[b] # yes, return the value
+        # id += ' (' + b + ')'
+        return id
+    except KeyError:
+        # print('(' + data + ')')
+        return '-1' # no, this must be either junk or not a class we offer
+
 def getCourseData(id):
+    global numberToID
+    global DEBUG
     #url = 'http://floridapolytechnic.catalog.acalog.com/ajax/preview_course.php?catoid=' + str(semester) + '&coid=' + id + '&display_options=a%3A2%3A%7Bs%3A8%3A~location~%3Bs%3A7%3A~program~%3Bs%3A4%3A~core~%3Bs%3A4%3A~9085~%3B%7D&show'
     url = 'file:C:/Users/Gabe/Documents/School/Etcetera/Scrapers/cache/preview_course.php.html&catoid=7&coid=' + id + ".html";
 
@@ -30,61 +47,60 @@ def getCourseData(id):
         a = soup.find('h3')
         b = a.string
         number = b.split(' - ')[0].replace(' ', '')
+        numberToID[number] = id
         name = b.split(' - ')[1]
 
         credits = int(soup.find('strong').next_element.next_element)
 
-        string = ''
+        # string = ''
+        # all of this... ALL. OF. THIS. To get a nice list of prereqs
         try:
             a = soup.find_all('div', {'class': 'ajaxcourseindentfix'})[1]
             b = a.find('p') # <p> containing prereq data
             c = b.text.split('Prerequisites: ')[1] # text containing class list
             d = str(c.encode('utf-8'))[2:-1] # remove anoying weird stuff at end
-            e = d.replace(',', ' and') # some use commas instead of and
-            prereqs = e.replace('\\xc2', ' ').replace('\\xa0', ' ').replace('\\xc3', ' ').replace('\\x82', ' ') # rmv weird chars
+            e = d.replace('\\xc2\\xa0', '').replace('\\xc3\\x82', ' - ') # rmv weird chars
+            f = e.replace(', ', ' and ').replace(' and or ', ' or ').replace(' and and ', ' and ') # some use commas instead of 'and'
+            g = f.replace(' AND ', ' and ').replace(' OR ', ' or ') # fix capitalization inconsistancies
+            # g = class data in string form
 
-            string += '\n\n-' + str(id) + '-----------------------------\n\n' + prereqs
+            # string += '\n\n-' + str(id) + '-----------------------------\n\n' + prereqs
 
-            # prereqs = soup.find_all('div', {'class': 'ajaxcourseindentfix'})[1]
-            # a = prereqs.text.split('Prerequisites: ')[1]
-            # b = a.split('Course Description:')[0]
-            # c = b.split('and')
-            # for d in c:
-            #     e = d.split('or')
-            #     # remove if bad
-            #     regex = re.compile("[a-zA-Z]{3}[0-9]{4}")
-            #     filtered = filter(lambda i: not regex.search(i), e)
-            #     filtered = [i for i in e if not regex.search(i)]
-            #     #print(filtered)
-            #     for x in filtered:
-            #         #if x in e:
-            #         e.remove(x)
-            #     #e = e - filtered
-            #
-            #     if len(e) > 1: string += '['
-            #     for f in range(len(e)):
-            #         g = e[f].split('-')[0].strip().replace(' ', '');
-            #         string += '"' + g + '"'
-            #         if f < len(e) - 1: string += ', '
-            #     if len(e) > 1: string += ']'
-            #     if c.index(d) < len(c) - 1:
-            #         string += ', '
-            #     else:
-            #         string += ']'
+            a = g.split(' and ') # each of the potential courses
+            array = '['
+            for b in a:
+                c = b.split(' or ') # occasionally, you have to deal with 'or'
+                if len(c) > 1: array += '['
+                for d in range(0, len(c)): # each of the classes, either AND or OR
+                    theId = getClassID(c[d])
+                    if theId != '-1':
+                        array += theId + ', '
+                if len(c) > 1: array = array[:-2] + '], '
+            array = array[:-2] + ']'
+            array = array.replace(',]', '')
+            array = array.replace('], ', '[')
+            try:
+                while array[0] == ']':
+                    array = array[1]
+            except IndexError:
+                array = ''
+            if array == '':
+                array = 'null'
+
+            prereqs = array
+            # prereqs = prereq data in str(array) form
+
+
         except IndexError:
-            print("index error")
+            prereqs = 'null'
+            if DEBUG: print('index error for id: ' + id)
             pass # doesn't even have Prerequisites
-        try:
-            pass
-            #m = re.search(pattern, string).group(0)
-        except AttributeError:
-            pass
-            #m = ''
-        print(string)
 
-        for i in soup.find_all('div'):
-            pass
-        return "hgegoieh"
+
+        # if id == '3023':
+        #   print(string)
+
+        return {'id': id, 'number': number, 'name': name, 'credits': credits, 'prereqs': prereqs}
 
 
 with urllib.request.urlopen(url) as response:
@@ -92,20 +108,18 @@ with urllib.request.urlopen(url) as response:
 
    soup = BeautifulSoup(html, "html.parser")
 
-   count = -5;
+   global numberToID
    for i in soup.find_all('li'):
        try:
            if i.attrs['class'][0] == 'acalog-course':
-               count += 1
-               if count > 5: break;
                a = i.contents[0]
                b = a.contents[0]
                c = b.attrs['onclick']
                d = c.replace(' ', '')
                e = d.split("','")[1]
                f = e.split(',')[0].split("'")[0]
-            #    if f != '2904': continue
                course = getCourseData(f)
-               #print(course)
+               print(course)
        except KeyError:
            pass
+   # print(numberToID)

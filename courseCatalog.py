577a317a03536351a6f364863d23cf9cd2981891
Gabriel Hutchison <ghutchison2600@flpoly.org>
2016-11-11 17:25:23 -0500

    Added support for multiple degrees/concentrations (not yet implemented)

--- courseCatalog.py
+++ courseCatalog.py
@@ -2,16 +2,16 @@ import urllib.request
 import html5lib
 from bs4 import BeautifulSoup
 import threading
+import re
+import json
 
 DEBUG = False;
 
 semester = 7;
-#url = 'http://floridapolytechnic.catalog.acalog.com/preview_program.php?catoid=' + str(semester) + '&poid=401&returnto=302'
-url = 'file:C:/Users/Gabe/Documents/School/Etcetera/Scrapers/cache/preview_program.php&catoid=7&poid=401.html'
 global numberToID
 numberToID = {}
-global courseList
-courseList = []
+global degrees
+degrees = {}
 
 def getCourseID(data):
     global numberToID
@@ -23,17 +23,16 @@ def getCourseID(data):
     except KeyError:
         return '-1' # no, this must be either junk or not a course we offer
 
-def courseIndForID(id):
+def courseIndForID(courseList, id):
     for courseInd in range(0, len(courseList)):
-        if courseList[courseInd]['id'] == id:
+        if str(courseList[courseInd]['id']) == str(id):
             return courseInd
     return -1
 
-def getCourseData(id):
+def getCourseData(degree, concentration, id):
     global numberToID
     global DEBUG
-    #url = 'http://floridapolytechnic.catalog.acalog.com/ajax/preview_course.php?catoid=' + str(semester) + '&coid=' + id + '&display_options=a%3A2%3A%7Bs%3A8%3A~location~%3Bs%3A7%3A~program~%3Bs%3A4%3A~core~%3Bs%3A4%3A~9085~%3B%7D&show'
-    url = 'file:C:/Users/Gabe/Documents/School/Etcetera/Scrapers/cache/preview_course.php.html&catoid=7&coid=' + id + ".html";
+    url = 'http://floridapolytechnic.catalog.acalog.com/ajax/preview_course.php?catoid=' + str(semester) + '&coid=' + id + '&display_options=a%3A2%3A%7Bs%3A8%3A~location~%3Bs%3A7%3A~program~%3Bs%3A4%3A~core~%3Bs%3A4%3A~9085~%3B%7D&show'
 
     with urllib.request.urlopen(url) as response:
         html = response.read()
@@ -66,29 +65,26 @@ def getCourseData(id):
             # g = course data in string form
 
             a = g.split(' and ') # each of the potential courses
-            array = '['
+            array = []
             for b in a:
                 c = b.split(' or ') # occasionally, you have to deal with 'or'
-                if len(c) > 1: array += '['
+                orray = []
                 for d in range(0, len(c)): # each of the courses, either AND or OR
                     theId = getCourseID(c[d])
                     if theId != '-1':
-                        array += theId + ', '
-                if len(c) > 1: array = array[:-2] + '], '
-            array = array[:-2] + ']'
-            array = array.replace(',]', '')
-            array = array.replace('], ', '[')
-            try:
-                while array[0] == ']':
-                    array = array[1]
-            except IndexError:
-                array = ''
-            if array == '':
-                array = 'null'
+                        orray.append(int(theId))
+                if len(orray) > 1:
+                    array.append(orray)
+                else:
+                    try:
+                        array.append(orray[0])
+                    except IndexError:
+                        # wasn't anything in orray, move on
+                        pass
 
             prereqs = array # prereqs = prereq data in str(array) form
         except IndexError:
-            prereqs = 'null'
+            prereqs = []
             if DEBUG: print('index error for id: ' + id)
             pass # doesn't even have Prerequisites
 
@@ -107,124 +103,167 @@ def getCourseData(id):
 
             h = g.strip().replace(' ', '', 1) # remove first space...
             i = h.split(' ')[0] # ...which should make this the course number
-            coreq = i # pass that because we haven't built the entire numberToID array yet
+            coreq = [i] # pass that because we haven't built the entire numberToID array yet
         except IndexError:
-            coreq = 'null'
+            coreq = []
             if DEBUG: print('index error for id: ' + id)
             pass # doesn't even have Co-requisites
 
-        courseList.append({'id': id, 'number': number, 'name': name, 'credits': credits, 'description': description, 'prereqs': prereqs, 'coreq': coreq, 'electivesInGroup': []})
+        degrees[degree][concentration].append({'id': int(id), 'number': number, 'name': name, 'credits': credits, 'description': description, 'prereqs': prereqs, 'coreq': coreq, 'electivesInGroup': []})
         return
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
+def getConcentrationData(url):
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        soup = BeautifulSoup(html, 'html5lib')
 
-    soup = BeautifulSoup(html, "html.parser")
+        # get degree title
+        for a in soup.find_all('p'): # degree in element
+            try:
+                b = a.a.text # degree title
+            except AttributeError:
+                if DEBUG: print('Not the title element; keep going')
+        degree = b
 
-    # build course list
-    for i in soup.find_all('li'):
-        try:
-            if i.attrs['class'][0] == 'acalog-course':
-                a = i.contents[0]
-                b = a.contents[0]
-                c = b.attrs['onclick']
-                d = c.replace(' ', '')
-                e = d.split("','")[1]
-                f = e.split(',')[0].split("'")[0]
+        # get concentration
+        a = soup.find('h1') # concentration title
+        b = a.text # text
+        concentration = b
 
-                # start a thread to load data asynchronously
-                # course = getCourseData(f)
-                t = threading.Thread(target=getCourseData, args=(f,))
-                t.start()
-                t.join()
-        except KeyError:
-            pass
+        if degree not in degrees:
+            degrees[degree] = {}
+        degrees[degree][concentration] = []
 
-    # post-process
-    # convert co-req temporary ids to co-req internal ids
-    for course in courseList:
-        try:
-            course['coreq'] = numberToID[course['coreq']]
-        except KeyError:
-            if DEBUG: print('No coreq in course ' + course['id'])
-            pass #
+        # build course list
+        for i in soup.find_all('li'):
+            try:
+                if i.attrs['class'][0] == 'acalog-course':
+                    a = i.contents[0]
+                    b = a.contents[0]
+                    c = b.attrs['onclick']
+                    d = c.replace(' ', '')
+                    e = d.split("','")[1]
+                    f = e.split(',')[0].split("'")[0]
 
-    # find elective groups
-    electiveGroups = [];
-    # find all courses that say "courseA" OR "courseB" (AKA electives in my book)
-    for i in soup.find_all('li', {'class': 'acalog-adhoc acalog-adhoc-after'}):
-        if i.strong.text.upper() == 'OR':
-            a = i.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element;
-            b = i.next_element.next_element.next_element.next_element.next_element;
-            group = [str(a).split("'")[3], str(b).split("'")[3]]
-            electiveGroups.append(group)
-    # find all courses that are in an HTML element grouping
-    for i in soup.find_all('div', {'class': 'acalog-core'}):
-        group = []
-        try:
-            a = i.h4.parent.find('ul')
-            b = str(a).split('showCourse') # logically close to each course id
-            for c in b:
-                try:
-                    d = c.split("'")[3] # the id (Staples: that was easy)
-                    group.append(d)
-                except IndexError:
-                    if DEBUG: print('This was junk data: ' + c)
-        except AttributeError:
-            if DEBUG: print('Not a group; move on')
-        if len(group) > 0:
-            electiveGroups.append(group)
+                    # start a thread to load data asynchronously
+                    t = threading.Thread(target=getCourseData, args=(degree,concentration,f,))
+                    t.start()
+                    t.join()
+            except KeyError:
+                pass
 
-    # apply electiveGroups to the courseList object
-    for group in electiveGroups:
-        for course in group:
-            ind = courseIndForID(course)
-            for addCourse in group:
-                if addCourse == course: continue
-                courseList[ind]['electivesInGroup'].append(addCourse)
+        courseList = degrees[degree][concentration]
+        # post-process
+        # convert co-req temporary ids to co-req internal ids
+        for course in courseList:
+            try:
+                course['coreq'] = [int(numberToID[course['coreq'][0]])]
+            except KeyError:
+                if DEBUG: print('No coreq in course ' + course['id'])
+            except IndexError:
+                pass # no coreqs, move on
 
-    # remove any electives if they are also co-reqs (i.e. Chem/Chem Lab)
-    for course in courseList:
-        courseID = course['coreq']
-        try:
-            course['electivesInGroup'].remove(courseID)
-        except ValueError:
-            if DEBUG: print(course['id'] + '\'s coreq (' + courseID + ') isn\'t an elective (' + str(course['electivesInGroup']) + '), but that\'s okay')
+        # find elective groups
+        electiveGroups = [];
+        # find all courses that say "courseA" OR "courseB" (AKA electives in my book)
+        for i in soup.find_all('li', {'class': 'acalog-adhoc acalog-adhoc-after'}):
+            if i.strong.text.upper() == 'OR':
+                a = i.previous_element.previous_element.previous_element.previous_element.previous_element.previous_element;
+                b = i.next_element.next_element.next_element.next_element.next_element;
+                group = [str(a).split("'")[3], str(b).split("'")[3]]
+                electiveGroups.append(group)
+        # find all courses that are in an HTML element grouping
+        for i in soup.find_all('div', {'class': 'acalog-core'}):
+            group = []
+            try:
+                a = i.h4.parent.find('ul')
+                b = str(a).split('showCourse') # logically close to each course id
+                for c in b:
+                    try:
+                        d = c.split("'")[3] # the id (Staples: that was easy)
+                        group.append(d)
+                    except IndexError:
+                        if DEBUG: print('This was junk data: ' + c)
+            except AttributeError:
+                if DEBUG: print('Not a group; move on')
+            if len(group) > 0:
+                electiveGroups.append(group)
 
-    # convert to JSON
-    out = '[\n'
-    space = ',\n        '
-    for course in courseList:
-        out += '    {\n        '
-        out += '"id": ' + course['id'] + space
-        out += '"number": "' + course['number'] + '"' + space
-        out += '"name": "' + course['name'] + '"' + space
-        out += '"credits": ' + str(course['credits']) + space
-        out += '"description": "' + course['description'] + '"' + space
-        out += '"prereqs": '
-        if course['prereqs'] == 'null':
-            out += '[]'
-        else:
-            out += course['prereqs']
-        out += space
-        out += '"coreqs": ['
-        if course['coreq'] != 'null':
-            out += course['coreq']
-        out += ']' + space
-        out += '"electivesInGroup": ['
-        for elective in course['electivesInGroup']:
-            out += elective + ', '
-        if len(course['electivesInGroup']) > 0:
-            out = out[:-2]
-        out += ']'
-        out += '\n    },\n'
-    if len(courseList) > 0:
-        out = out[:-2]
-    out += '\n]\n'
+        # apply electiveGroups to the courseList object
+        for group in electiveGroups:
+            for course in group:
+                ind = courseIndForID(courseList, course)
+                for addCourse in group:
+                    if addCourse == course: continue
+                    courseList[ind]['electivesInGroup'].append(int(addCourse))
 
-    # output to file
-    with open('output.json', 'w') as fileObj:
-        fileObj.write(out)
-        fileObj.close()
+        # remove any electives if they are also co-reqs (i.e. Chem/Chem Lab)
+        for course in courseList:
+            try:
+                courseID = course['coreq'][0]
+            except IndexError:
+                continue # no coreq, so just move one
+            try:
+                course['electivesInGroup'].remove(courseID)
+            except ValueError:
+                if DEBUG: print(course['id'] + '\'s coreq (' + courseID + ') isn\'t an elective (' + str(course['electivesInGroup']) + '), but that\'s okay')
 
-    print('Data output to output.json')
+        # convert to JSON
+        # out = '[\n'
+        # space = ',\n        '
+        # for course in courseList:
+        #     out += '    {\n        '
+        #     out += '"id": ' + course['id'] + space
+        #     out += '"number": "' + course['number'] + '"' + space
+        #     out += '"name": "' + course['name'] + '"' + space
+        #     out += '"credits": ' + str(course['credits']) + space
+        #     out += '"description": "' + course['description'] + '"' + space
+        #     out += '"prereqs": '
+        #     if course['prereqs'] == 'null':
+        #         out += '[]'
+        #     else:
+        #         out += course['prereqs']
+        #     out += space
+        #     out += '"coreqs": ['
+        #     if course['coreq'] != 'null':
+        #         out += course['coreq']
+        #     out += ']' + space
+        #     out += '"electivesInGroup": ['
+        #     for elective in course['electivesInGroup']:
+        #         out += elective + ', '
+        #     if len(course['electivesInGroup']) > 0:
+        #         out = out[:-2]
+        #     out += ']'
+        #     out += '\n    },\n'
+        # if len(courseList) > 0:
+        #     out = out[:-2]
+        # out += '\n]\n'
+
+        degrees[degree][concentration] = courseList
+
+
+# code starts running here
+print("Loading...")
+for i in range(0, 1):
+    url = 'http://floridapolytechnic.catalog.acalog.com/preview_program.php?catoid=' + str(semester) + '&poid=401'
+
+    # start a thread to load data asynchronously
+    t = threading.Thread(target=getConcentrationData, args=(url,))
+    t.start()
+    t.join()
+
+# for d in degrees:
+#     print(d)
+#     for c in degrees[d]:
+#         print(c)
+#         for crs in range(0, len(degrees[d][c])):
+#             print(crs)
+#             degrees[d][c][crs] = json.loads(degrees[d][c][crs])
+
+jsonData = json.dumps(degrees, indent=4)
+
+# output to file
+with open('output.json', 'w') as fileObj:
+    fileObj.write(jsonData)
+    fileObj.close()
+print('Data output to output.json')
